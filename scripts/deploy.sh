aea28f3d70bc4c3d6962811f63c65777bef8a89c
froglegg <hayescrowley@gmail.com>
2023-12-24 09:56:19 -0500

    generalize organization name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 # login to azure if you haven't already
 az login
-az acr login --name sdcc2023team8registry
+az acr login --name my_teamregistry
 
 # build go binaries for linux Alpine (go server, etc.)
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ../bin/manic-server ../web/manic-server/...
@@ -15,14 +15,14 @@ npm run --prefix ../web/manic-client/ build
 docker compose -f ../docker/docker-compose.yml build
 
 # build, tag and push images to ACR
-docker tag manic-server:latest sdcc2023team8registry.azurecr.io/manic-server:latest
-docker push sdcc2023team8registry.azurecr.io/manic-server:latest
-docker tag manic-client:latest sdcc2023team8registry.azurecr.io/manic-client:latest
-docker push sdcc2023team8registry.azurecr.io/manic-client:latest
+docker tag manic-server:latest my_teamregistry.azurecr.io/manic-server:latest
+docker push my_teamregistry.azurecr.io/manic-server:latest
+docker tag manic-client:latest my_teamregistry.azurecr.io/manic-client:latest
+docker push my_teamregistry.azurecr.io/manic-client:latest
 
 # restart the web app
-az webapp restart --name manic-compression --resource-group team8
-az webapp show --name manic-compression --resource-group team8 --query state
+az webapp restart --name manic-compression --resource-group my-team
+az webapp show --name manic-compression --resource-group my-team --query state
 
 echo "Deployment complete!"
 echo "Navigate to http://manic-compression.azurewebsites.net/ to view the app."
